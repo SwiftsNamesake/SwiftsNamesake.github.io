4789dfa0bf6ecd8d8d606d7a5f0c9fb07174a0fe
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2015-07-03 00:40:41 +0200

    debugging

--- js/WaveFront.js
+++ js/WaveFront.js
@@ -297,7 +297,7 @@ var WaveFront = (function() {
 		console.log(OBJ.faces.length)
 		console.log(OBJ.faces[0].vertices.length);
 
-		var vertices  = OBJ.faces.map(function(f) { return f.vertices.slice(0, 3).map(function(v) { return OBJ.vertices[v]; }); });
+		var vertices  = OBJ.faces.map(function(f) { return f.vertices.slice(0, 3).map(function(v) { return OBJ.vertices[v]; }).reverse(); });
 		var normals   = OBJ.faces.map(function(f) { return f.normals.slice(0, 3).map(function(n)  { return OBJ.normals[n];  }); });
 
 		var colours   = OBJ.faces.map(function(f) {
