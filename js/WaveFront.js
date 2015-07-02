4ff7b663226e01c526e2a887fdc9ea8bf8e69a2c
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2015-07-02 21:36:32 +0200

    Think I may possibly have finished the parsers and model loader

--- js/WaveFront.js
+++ js/WaveFront.js
@@ -10,11 +10,19 @@
  *        - Format debugging messages
  *        - Support multiple MTL files (part of standard?) (?)
  *        - Figure out resource dependencies (MTL/textures/etc.)
- *          -- Include a list of mtl dependencies in the return object (?)
+ *          -- Include a list of MTL dependencies in the return object (?)
  *          -- High-level API function for loading an OBJ file and related material files
  *
  *        - Support index arrays (setting?)
  *        - Optimise, profile
+ *
+ *        - Decide on an API
+ *          -- Hide internal utility functions (?)
+ *
+ *        - Fix parser bugs
+ *          -- Formal spec compliance
+ *          -- Support coordinates inside parentheses
+ *          --
 
  * SPEC | -
  *        -
@@ -73,7 +81,7 @@ var WaveFront = (function() {
 		for (var line of source.split(/\n+/).filter(WaveFront.notComment)) {
 			
 			// TODO: Decide between abbreviated and full-length keys
-			console.log(line);
+			// console.log(line);
 
 			// For each line that is not blank or a comment
 			var values = line.split(/\s+/); // Split on space
@@ -92,10 +100,11 @@ var WaveFront = (function() {
 			} else if (WaveFront.contains(values[0], ['Ns', 'Ni', 'd', 'Tr', 'illum'])) {
 				// ?, ?, d(issolved), Tr(ansparent), ?
 				// Dissolved and Transparent are synonyms
-				// console.log('The valid MTL property \'{0}\' is currently unsupported by this parser and will have no effect.'.format(values[0]));
+				console.log('The valid MTL property \'{0}\' is currently unsupported by this parser and will have no effect.'); //.format(values[0]));
 			} else {
 				// Unknown parameter encountered
-				// throw ValueError('\'{0}\' is not a recognised parameter.'.format(values[0]))
+				console.log('\'{0}\' is not a recognised parameter.'); //.format(values[0]))
+				// throw ValueError('\'{0}\' is not a recognised parameter.'); //.format(values[0]))
 			}
 
 		}
@@ -127,21 +136,17 @@ var WaveFront = (function() {
 
 		"use strict";
 
-		var data = { vertices: [], normals: [], textures: [], faces: [], groups: [], mtl: undefined, material: undefined }; //
-		// var path 	= parent(filename);  // Path to containing folder
-
-		/*var lookup = {
-			'v': undefined,
-			'vn': undefined,
-			'vt': undefined,
-			'f': undefined,
-			'g': undefined,
-			'o': undefined,
-			's': undefined,
-			'mtllib': undefined,
-			'usemtl': undefined,
-			['l']: undefined
-		}*/
+		// TODO: Rename mtl (?)
+		// TODO: Separate intermediary data (the components of the 'state machine', eg. mtl, material) and the final output (?)
+		var data = { vertices:  [], // Unique vertices
+		             normals:   [], // Unique normal vectors
+		             texcoords: [], // Unique texture coordinates
+		             faces:     [], // Faces ([{vertices: [[Double]], normals: [[Double]], texcoords: [[Double]], material: String })
+		             groups:    [], // List of group objects ({name: String, lower: Int}), converted to a dictionary ({name: { lower: Int, upper: Int }}) at the end
+		             mtl:       undefined,    // Current MTL file (as defined by the most recent mtllib statement)
+		             material:  undefined,    // Current MTL material name (as defined by the most recent usemtl statement)
+		             dependencies: new Set() // A set of the names of all MTL dependencies for this OBJ model
+		           };
 
 		for (var line of source.split(/\n+/).filter(WaveFront.notComment)) {
 
@@ -150,49 +155,50 @@ var WaveFront = (function() {
 			if (values[0] == 'v') {
 				// Vertex coordinates
 				data.vertices.push(values.slice(1, 4).map(parseFloat)); // TODO: Handle invalid vertex data
-			} else if (values[0] == 'vn') {
+			} else if (values[0] === 'vn') {
 				// Vertex normal
 				data.normals.push(values.slice(1, 4).map(parseFloat)); // TODO: Handle invalid normal data
-			} else if (values[0] == 'vt') {
+			} else if (values[0] === 'vt') {
 				// Texture coordinates
-				data.textures.push(values.slice(1, 3).map(parseFloat)); // TODO: Handle invalid texture data
-			} else if (values[0] == 'f') {
+				data.texcoords.push(values.slice(1, 3).map(parseFloat)); // TODO: Handle invalid texture data
+			} else if (values[0] === 'f') {
 				// Face
 				// TODO: Save indeces instead (would probably save memory) (?)
 				// TODO: Refactor (?)
 				// TODO: Handle absent values for normals and texture coords (✓)
 				// TODO: Handle vertex definitions of varying length (eg. 50/2/1 55/2 60) ()
 				var face = values.slice(1).map(function(vertex) { return vertex.split(/\//) }); // Extract indices for each vertex of the face
-				console.assert(face.every(function(vertex) { return vertex.length == face[0].length; }));
+				console.assert(face.every(function(vertex) { return vertex.length === face[0].length; }));
 
-				data.faces.push({ vertices:  face.map(function(vertex) { return data.vertices[parseInt(vertex[0])-1]; }), 							// Vertices
-									texcoords: face.map(function(vertex) { return face[0].length > 1 ? data.textures[parseInt(vertex[1])-1] : null; }), // Texture coordinates
-									normals:   face.map(function(vertex) { return face[0].length > 2 ? data.normals[parseInt(vertex[2])-1]  : null; }), // Normals
-									material:  data.material }); 																	                    // Material
-			} else if (values[0] == 'g') {
+				data.faces.push({ vertices:  face.map(function(vertex) { return parseInt(vertex[0])-1; }),                             // Vertices
+								  texcoords: face.map(function(vertex) { return face[0].length > 1 ? parseInt(vertex[1])-1 : null; }), // Texture coordinates
+								  normals:   face.map(function(vertex) { return face[0].length > 2 ? parseInt(vertex[2])-1 : null; }), // Normals
+								  material:  data.material });                                                                         // Material
+			} else if (values[0] === 'g') {
 				// Group
 				// console.log('Adding group:', values[2])
 				// TODO: Use object instead of a two-item array (?) (✓)
-				data.groups.push({ name: values[2], lower: len(data['faces']) }) // Group name with its lower bound (index into faces array)
-			} else if (values[0] == 'o') {
+				data.groups.push({ name: values[2], lower: data.faces.length }) // Group name with its lower bound (index into faces array)
+			} else if (values[0] === 'o') {
 				// Object
-				// console.log('Ignoring OBJ property \'{0}\''.format(values[0]));
-			} else if (values[0] == 's') {
+				console.log('Ignoring OBJ property \'{0}\''); //.format(values[0]));
+			} else if (values[0] === 's') {
 				// Smooth shading
-				// console.log('Ignoring OBJ property \'{0}\''.format(values[0]));
-			} else if (values[0] == 'mtllib') {
+				console.log('Ignoring OBJ property \'{0}\''); //.format(values[0]));
+			} else if (values[0] === 'mtllib') {
 				// MTL library
-				// Load materials defined in an external file
 				data.mtl = values[1]; //WaveFront.parseMTL(join(path, values[1])); // TODO: FIX PATH HANDLING!!!!!
-			} else if (values[0] == 'usemtl') {
+				data.dependencies.add(values[1]);
+			} else if (values[0] === 'usemtl') {
 				// Use MTL material
 				// TODO: Handle usemtl (null)
 				data.material = { file: data.mtl, material: values[1] }; //data.mtl[values[1]] // Current material
 			} else if (WaveFront.contains(values[0], ['l'])) {
-				// console.log('Unsure how to handle property \'{0}\'.'.format(values[0]));
+				console.log('Unsure how to handle property \'{0}\'.'); //.format(values[0]));
 			} else {
 				// Unknown parameter encountered
-				// throw ValueError('\'{0}\' is not a recognised parameter.'.format(values[0]));
+				console.log('\'{0}\' is not a recognised parameter.'); //.format(values[0]))
+				// throw ValueError('\'{0}\' is not a recognised parameter.'); //.format(values[0]))
 			}
 		
 		}
@@ -200,8 +206,8 @@ var WaveFront = (function() {
 		// TODO: Handle data with no group definitions
 		var prev = data.groups.length;
 		// console.log('Groups', data.groups)
-		// console.assert(data.groups.length == 0 || data.groups[0][1] == 0, 'All faces must belong to a group. (lowest index is {0})'.format(data.groups[0][1]))
-		console.assert(data.groups.length == 0 || data.groups[0][1] == 0, 'All faces must belong to a group. (lowest index is ' + data.groups.lower + ')')
+		console.assert(data.groups.length === 0 || data.groups[0][1] === 0, 'All faces must belong to a group. (lowest index is {0})'); //.format(data.groups[0][1]))
+		console.assert(data.groups.length === 0 || data.groups[0][1] === 0, 'All faces must belong to a group. (lowest index is ' + data.groups.lower + ')')
 
 		
 		// Map group names to their lower and upper bounds
@@ -220,7 +226,7 @@ var WaveFront = (function() {
 
 		data.groups = bounded;
 
-		// assert len(data.groups) == prev
+		// assert len(data.groups) === prev
 		// console.log('Groups', data.groups)
 
 		// Meta data
@@ -228,7 +234,7 @@ var WaveFront = (function() {
 		// TODO: Return an object instead, to facilitate marshalling (?)
 		data.meta = {};
 
-		for (let attribute of ['vertices', 'normals', 'faces']) {
+		for (var attribute of ['vertices', 'normals', 'faces']) {
 			data.meta[attribute] = data[attribute].length;
 		}
 
@@ -239,6 +245,59 @@ var WaveFront = (function() {
 
 	WaveFront.loadOBJ = function(fn) { return $.ajax(fn).then(WaveFront.parseOBJ); };
 	WaveFront.loadMTL = function(fn) { return $.ajax(fn).then(WaveFront.parseMTL); };
+
+	WaveFront.loadMesh = function(fn, path) {
+
+		//
+		// TODO: Assume the MTL files are in the same directory (?)
+		console.log('Loading OBJ file: ', fn);
+		console.log('Path to MTL files is: ', path);
+
+		return WaveFront.loadOBJ(fn).then(function(OBJ) {
+
+			//
+			var count = OBJ.dependencies.size;
+			var numerals = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve'];
+			console.log('Finished loading OBJ file: ', fn);
+			console.log('Loading ' + (numerals[count] || count)  + ' MTL dependenc' + (count === 1 ? 'y' : 'ies') + '.');
+
+			var dependencies = [];
+			for (var name of OBJ.dependencies) {
+				// Make sure the callback inside the loop is not affected by scoping issues w.r.t 'name'.
+				dependencies.push(WaveFront.loadMTL(path + name).then(function(MTL) {
+					console.log('Loaded MTL file: ', name);
+					return { name: name, data: MTL };
+				}));
+			}
+
+			$.when.apply($, dependencies).then(function() {
+				var MTLs = {};
+				for (var MTL of arguments) {
+					MTLs[MTL.name] = MTL.data;
+				}
+
+				return WaveFront.createMeshes(OBJ, MTLs);
+
+			});
+		});
+
+	};
+
+
+	WaveFront.createMeshes = function(context, OBJ, MTLs) {
+
+		// Creates a dictionary of mesh objects ({groupname: mesh}) from parsed OBJ and MTL data
+		// TODO: Optimise arrays
+		// TODO: Use index buffers
+		// TODO: Support textures
+		var vertices  = OBJ.faces.map(function(f) { return f.vertices.map(function(v) { return OBJ.vertices[v]; }); }).merge();
+		var normals   = OBJ.faces.map(function(f) { return f.normals.map(function(n)  { return OBJ.normals[n];  }); }).merge();
+		var colours   = OBJ.faces.map(function(f) { return MTLs[f.material.file][f.material.material]['Ka']; }).merge();
+		// var texcoords = OBJ.faces.map(function(f) { return f.texcoords.map(function(t) { return OBJ.texcoords[t]; }); }).merge();
+
+		return new Mesh(context, { vertices: vertices, colours: colours });
+
+	};
 
 
 	return WaveFront;
