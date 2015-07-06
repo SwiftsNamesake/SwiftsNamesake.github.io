0abdfba16a718944ccfed993d4236a173fe6dced
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2015-07-06 21:51:53 +0200

    bugfix

--- js/WaveFront.js
+++ js/WaveFront.js
@@ -326,7 +326,7 @@ var WaveFront = (function() {
 			// return WaveFront.tessellate(...).flatten();
 			return WaveFront.tessellate(f.vertices.map(function(v) {
 				return OBJ.vertices[v];
-			})).flatten();
+			}))
 			// return f.vertices.slice(0,3).map(function(vi) { return OBJ.vertices[vi]; }); // Transform vertex indices to vertex coordinates
 		}).flatten();
 
