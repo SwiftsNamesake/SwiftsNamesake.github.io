ad85a8e9c7023eb6fd0a95fab7bb05428f005d87
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2015-07-10 21:39:47 +0200

    debugging

--- js/Mesh.js
+++ js/Mesh.js
@@ -66,6 +66,7 @@ var Mesh = function(context, data, name) {
 			});
 		}
 	} else {
+		console.log(this.name + 'is not textured. Creating dummy buffer...');
 		this.texcoords = context.createBuffer(data.vertices.map(function(_) { return [0,0] }).flatten(), 2);
 	}
 
