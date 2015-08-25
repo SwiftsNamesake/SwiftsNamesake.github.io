8bdd4583b6692cda5e4f0f6e793ab8d4a8e0ada1
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2015-08-25 02:54:43 +0200

    Too much to list. See diff.

--- js/index.js
+++ js/index.js
@@ -39,12 +39,12 @@ $(document).ready(function() {
 	});
 
 
-	var margin = random.choice(['margin-top', 'margin-bottom', 'margin-left', 'margin-right'])
-	$('div.page').css(margin, '100%');
-	setTimeout(function() {
-		$('div.page').css('-webkit-transition', margin + ' 1.2s ease').css(margin, '0%'); 
-		new Audio('data/audio/swooshing.mp3').play();
-	}, 2.5*1000)
+	// var margin = random.choice(['margin-top', 'margin-bottom', 'margin-left', 'margin-right'])
+	// $('div.page').css(margin, '100%');
+	// setTimeout(function() {
+	// 	$('div.page').css('-webkit-transition', margin + ' 1.2s ease').css(margin, '0%'); 
+	// 	new Audio('data/audio/swooshing.mp3').play();
+	// }, 2.5*1000)
 
 });
 
